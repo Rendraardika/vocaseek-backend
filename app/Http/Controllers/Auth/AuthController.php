213f8757c53d1c0ac2f18ee253dc86ebbca4ad62
rendra ardika <ardikarendra50@gmail.com>
2026-04-22 10:51:20 +0700

    Optimize login email validation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $request->validate([
-            'email' => 'required|email:rfc,dns',
+            'email' => 'required|email:rfc',
             'password' => 'required',
         ]);
 
